46acb281ae4844296944f5ccfc57796160fc8fd9
Dhruv Nilkund <hadoop@Ubuntu.myguest.virtualbox.org>
2023-09-02 12:12:55 +0530

    second commit

--- script.sh
+++ script.sh
@@ -1,12 +1,14 @@
+#!/usr/bin/env bash
 # Task 1: 
 # The first argument provided from cmd must be the language you are running it in
 # Just plainly print the first argument that was provided 
 # The second argument is the name of the python/C file that must be run 
 # Run the python file, using input1.txt as stdin, and write the output to a file user_output1.txt 
 # Display user_output1.txt to the user as user output, and the file output1.txt as the expected output 
-# Once you have tested out the script and verified it's working, commit it to a branch: <your_name>
-language= $1
-echo "Te language passed as arguement is $language"
+# Once you have tested out the script and verified it's working, commit it to a branch: 
+
+language=$1
+echo "The language passed as arguement is $language"
 cat input1.txt | python3 $2>user_output1.txt
 echo "User output:"
 cat user_output1.txt
